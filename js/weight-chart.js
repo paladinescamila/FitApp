89a9fd7513e7d9baf31ccfda09d9185b9e5075b5
paladinescamila <camilapaladines27@gmail.com>
2021-10-02 21:46:00 -0500

    Add chart 1

---
+++ js/weight-chart.js
@@ -0,0 +1,24 @@
+var weightChartCtx = document.getElementById("weight-chart").getContext("2d");
+
+var myChart = new Chart(weightChartCtx, {
+	type: "line",
+	data: {
+		labels: ["01/2021", "02/2021", "03/2021", "04/2021", "05/2021", "06/2021"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
+				borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+});
