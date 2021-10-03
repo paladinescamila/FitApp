74d5d4e255311eaad3adeee888111c93e276cbc4
paladinescamila <camilapaladines27@gmail.com>
2021-10-03 00:58:10 -0500

    Added dashboard page

--- js/IMC-chart.js
+++ js/IMC-chart.js
@@ -1,8 +1,8 @@
-const drawWeight = (weights, target, month, year) => {
-	let weightCtx = document.getElementById("weight-chart").getContext("2d"),
+const drawIMC = (IMC, target, month, year) => {
+	let IMCctx = document.getElementById("IMC-chart").getContext("2d"),
 		months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
 		labels = [],
-		maxValue = Math.max(...weights) + 20;
+		maxValue = Math.max(...IMC) + 10;
 
 	for (let i = 0; i < 12; i++) {
 		month = (month + 1) % 12;
@@ -10,22 +10,22 @@ const drawWeight = (weights, target, month, year) => {
 		if (month === 11) year++;
 	}
 
-	return new Chart(weightCtx, {
+	return new Chart(IMCctx, {
 		type: "line",
 		data: {
 			labels: labels,
 			datasets: [
 				{
 					type: "line",
-					label: "Peso (Kg)",
-					data: weights,
-					backgroundColor: "rgba(255, 132, 0, 0.3)",
-					borderColor: "rgb(255, 132, 0)",
+					label: "IMC",
+					data: IMC,
+					backgroundColor: "rgba(176, 28, 5, 0.3)",
+					borderColor: "rgb(176, 28, 5)",
 					borderWidth: 1,
 				},
 				{
 					type: "line",
-					label: "Peso objetivo (Kg)",
+					label: "IMC objetivo",
 					data: [target, target, target, target, target, target, target, target, target, target, target, target],
 					fill: false,
 					borderColor: "rgb(0, 0, 0)",
@@ -41,4 +41,8 @@ const drawWeight = (weights, target, month, year) => {
 	});
 };
 
-drawWeight([80, 78, 75, 70, 74], 60, 6, 2021);
+let height = 1.62,
+	weighs = [80, 78, 75, 70, 74],
+	IMC = weighs.map((w) => w / (height * height));
+
+drawIMC(IMC, 24.9, 6, 2021);
