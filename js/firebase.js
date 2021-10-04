ab68c383b07e71674d322d1937da3d38f03b86f5
paladinescamila <camilapaladines27@gmail.com>
2021-10-04 16:47:20 -0500

    Simplified html code

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDqj3-QL0NH92lEqGGCZRHiUTnfXdtV_W8",
+	authDomain: "fitappsoft.firebaseapp.com",
+	projectId: "fitappsoft",
+	storageBucket: "fitappsoft.appspot.com",
+	messagingSenderId: "979155495236",
+	appId: "1:979155495236:web:c8bf225edbfcdfdcd00f11",
+	measurementId: "G-797PJGJ085",
+};
+firebase.initializeApp(firebaseConfig);
+
 // firebase 7.15.1
 const db = firebase.firestore();
 
