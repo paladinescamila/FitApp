dfe6c057f109baeed0918cdb884d39894b422f1d
Nicolas Delgado <nsdr1228@gmail.com>
2021-10-09 01:33:19 -0500

    Fixing some UX features

--- js/index.js
+++ js/index.js
@@ -18,9 +18,7 @@ const searchUser = (user, pwd, cent) => {
     q.get().then((querySnapshot) => {
         querySnapshot.forEach(async (doc) => {
             if (doc.data().pwd != pwd && flag) {
-                alert(
-                    "Usuario o contraseña incorrectos, por favor, intente de nuevo"
-                );
+                alert("Usuario o contraseña incorrectos, por favor, intente de nuevo");
                 // document.getElementById("user").value = ""
                 // document.getElementById("password").value = ""
                 flag = false;
@@ -29,43 +27,48 @@ const searchUser = (user, pwd, cent) => {
                 userOnline(user);
 
                 await new Promise((r) => setTimeout(r, 1500));
-                window.location.replace(
-                    window.location.href.slice(
-                        0,
-                        window.location.href.indexOf("index.html")
-                    ) +
-                        "/pages/dashboard.html?user=" +
-                        user
-                );
-                console.log("Entramos!");
+                findBirthDate(user);
+                // console.log("Entramos!");
                 flag = false;
             }
         });
     });
 };
 
+const findBirthDate = (user) => {
+    db.collection("info")
+        .where("user", "==", user)
+        .get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                if (doc.data().birth === undefined) {
+                    //lleveme a form
+                    window.location.replace(window.location.href.slice(0, window.location.href.indexOf("index.html")) + "/pages/form.html?user=" + user);
+                } else {
+                    //lleveme a dashboard
+                    window.location.replace(window.location.href.slice(0, window.location.href.indexOf("index.html")) + "/pages/dashboard.html?user=" + user);
+                }
+            });
+        });
+};
+
 const findUserLogin = (user, pwd) => {
     const collectionRef = db.collection("users");
     try {
-        const response = collectionRef
-            .where("user", "==", user)
-            .onSnapshot((snapshot) => {
-                if (snapshot.docs.length === 0) {
-                    alert(
-                        "El usuario no se encuentra creado, por favor, registrese!"
-                    );
-                } else {
-                    searchUser(user, pwd, true);
-                }
-            });
+        const response = collectionRef.where("user", "==", user).onSnapshot((snapshot) => {
+            if (snapshot.docs.length === 0) {
+                alert("El usuario no se encuentra creado, por favor, registrese!");
+            } else {
+                searchUser(user, pwd, true);
+            }
+        });
     } catch (error) {
         console.log(error);
     }
 };
 
 function validateEmail(email) {
-    const re =
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
